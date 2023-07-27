27d7640521fd8336162e52f8d79df94be4eb1ed5
Marko Rajevic <me@markorajevic.space>
2023-07-27 13:24:07 +0200

    Log error

--- src/lib/services/image-loader.service.ts
+++ src/lib/services/image-loader.service.ts
@@ -404,6 +404,7 @@ export class ImageLoaderService {
                 done();
                 this.maintainCacheSize();
             } catch (err) {
+                console.log(err);
                 error(err);
                 throw err;
             }
